5dfdd14033e741b1cdba1aeb301d036a58f6caa7
hallucinogenizer <rohanhussain1@yahoo.com>
2025-07-04 17:26:23 +0500

    Created basic web server with one endpoint

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+const {Client} = require("pg")
+
+const PORT = 3050;
+
+app.get("/", async (req, res) => {
+    const client = new Client({
+        host: "localhost",
+        port: "5432",
+        user: "postgres",
+        password: "mysecretpassword",
+        database: "teaching2"
+    });
+    await client.connect();
+
+    const data = await client.query("SELECT * FROM Students;");
+
+    res.json(data.rows);
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`)
+})
